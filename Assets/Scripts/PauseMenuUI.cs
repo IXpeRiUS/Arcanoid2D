04b743e98b9345a621e847e66f55dfc3c50e87bc
Nikita Doynikov <ndoynikov@yandex.ru>
2024-10-17 15:13:03 +0500

    скорректировать коллайдеры у кирпичиков, скорректировать угол стен

---
+++ Assets/Scripts/PauseMenuUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+public class PauseMenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseMenuUI;
+
+    private void Start()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.onPause += TogglePauseMenu;
+            //Debug.Log("Event PAUSE enabled");
+        }
+        else
+        {
+            Debug.LogError("GameManager instance is null. Ensure it is initialized before PauseMenuUI.");
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.onPause -= TogglePauseMenu;
+        }
+    }
+
+    private void TogglePauseMenu(bool pause)
+    {
+        // Включить меню паузы, если игра на паузе, и выключить, если пауза отключена
+        if (pause)
+        {
+            _pauseMenuUI.SetActive(true); // Включить меню паузы
+            //Debug.Log("PauseON");
+        }
+        else
+        {
+            _pauseMenuUI.SetActive(false); // Выключить меню паузы
+            //Debug.Log("PauseOFF");
+        }
+    }
+}
